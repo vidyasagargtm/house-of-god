3b11c2fc85c7d1b0218bc80cb7c19cf57eb130f0
Vidyasagar Goutam <vidyasagargtm@gmail.com>
2018-12-28 17:52:31 +0530

    redme file

--- house_of_god/assignment/helper.py
+++ house_of_god/assignment/helper.py
@@ -12,11 +12,9 @@ def upload_csv():
     for i, item in enumerate(tdata.dict, start=1):
         post_id = int(item['Post ID'])
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(item[' Timestamp'])))
-        print(timestamp)
         data = {'post_id': post_id, 'timestamp': timestamp}
         serializer = serializers.PostSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            print(serializer.data)
         else:
             print('saga', serializer.errors)
